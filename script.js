0dea355636bff3acb93022467550ab90bf222cbe
krisnaputra31 <81416312+krisna31@users.noreply.github.com>
2023-02-06 10:05:33 +0700

    add 50px in canvas height

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth, window.innerHeight + 50);
 camera.position.setZ(30);
 camera.position.setX(-3);
 renderer.render(scene, camera);
